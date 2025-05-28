511f670e0ca9d6bd7ca9fc347eb60de6d0a48923
27qbracken09 <96312942+27qbracken09@users.noreply.github.com>
2025-05-28 11:01:15 -0600

    Hack Commit

--- 2131YPushBack/src/Chassis/ControllerInput.cpp
+++ 2131YPushBack/src/Chassis/ControllerInput.cpp
@@ -9,8 +9,9 @@ ControllerJoystick::ControllerJoystick(pros::controller_analog_e_t Joystick, int
 double ControllerJoystick::value(){
     double value;
     (std::fabs(mainController.get_analog(m_joystick)) > m_deadZone) ? value = mainController.get_analog(m_joystick) : value = 0;
+
     return value;
 }
 
-ControllerJoystick velocityJoystick(pros::E_CONTROLLER_ANALOG_LEFT_Y, 0);
-ControllerJoystick turningJoystick(pros::E_CONTROLLER_ANALOG_RIGHT_X, 0);
+ControllerJoystick velocityJoystick(pros::E_CONTROLLER_ANALOG_LEFT_Y, 50);
+ControllerJoystick turningJoystick(pros::E_CONTROLLER_ANALOG_RIGHT_X, 50);
