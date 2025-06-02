024abd9a4f31881fa6e5d50ba158c6f6f3d165c3
Ezra Jones <120685907+ezraJones@users.noreply.github.com>
2025-06-02 16:23:10 -0600

    Kalman Filter Beginnings

---
+++ 2131YPushBack/include/Utilities/KalmanFilter.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+
+class KalmanFilter
+{
+    private:
+
+        // Class Member Variables
+        double m_measurementVariance;
+};
--- 2131YPushBack/include/Utilities/mathUtils.hpp
+++ 2131YPushBack/include/Utilities/mathUtils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <math.h>
+#include <vector>
+#include <numeric>
 
 
 #define toDeg(rad) (rad * 180/M_PI)
@@ -8,3 +10,18 @@
 
 double wrapAngle(double angle, bool degrees = true);
 double constrainAccel(double targetVelocity, double currentVelocity, double maxAccel);
+
+
+/**
+ * @brief Average any Set of Values
+ * 
+ * @tparam T 
+ * @param averageValues 
+ * @return T 
+ */
+template <typename T>
+T average(std::vector<T> averageValues)
+{
+    T summation = std::accumulate(averageValues.begin(), averageValues.end(), 0.0);
+    return summation / (averageValues.size());
+}
