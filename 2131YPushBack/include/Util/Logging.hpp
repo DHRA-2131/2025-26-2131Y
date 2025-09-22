282a94919aee9c671603e09ef7b2f6455249b2cd
27qbracken09 <96312942+27qbracken09@users.noreply.github.com>
2025-09-22 17:28:35 -0600

    Started Logger

--- 2131YPushBack/include/Util/Logging.hpp
+++ 2131YPushBack/include/Util/Logging.hpp
@@ -8,3 +8,68 @@
  * @copyright Copyright (c) 2025
  * 
  */
+
+#include <cstdint>
+#include <string>
+class Logger 
+ {
+    public:
+        enum class debugLevel; //Forward Declaration to make Logger happy
+        
+        /**
+         * @brief Construct a new Logger object
+         * 
+         * @param Level 
+         */
+        Logger(debugLevel Level = debugLevel::Status);
+
+        /**
+         * @brief Output message to logger
+         * 
+         * @param message 
+         */
+        void output(std::string message);
+
+        /**
+         * @brief Attach a variable to the logger process to be watched
+         * 
+         * @param varName 
+         * @param variable 
+         * @return uint8_t 
+         */
+        uint8_t attachVar(std::string varName, auto& variable);
+
+        /**
+         * @brief Output a var
+         * 
+         * @param varIdentifier 
+         */
+        void outputVar(uint8_t varIdentifier);
+
+        /**
+         * @brief Output a var
+         * 
+         * @param varName 
+         */
+        void outputVar(std::string varName);
+
+        /**
+         * @brief Output ALL vars
+         * 
+         */
+        void outputAllVars();
+
+
+        enum class debugLevel
+        {
+            None,
+            Warning,
+            Status,
+            Information,
+            Debug
+        };
+
+        friend std::ostream& operator<<(std::ostream& os, const Logger& printLogger);
+ };
+
+ std::ostream& operator<<(std::ostream& os, const Logger& printLogger);
