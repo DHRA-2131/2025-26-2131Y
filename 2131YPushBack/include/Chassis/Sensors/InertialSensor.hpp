75bcc5a05c0ff071f2f8952d8a6593ca00b14bf1
27qbracken09 <96312942+27qbracken09@users.noreply.github.com>
2025-09-22 16:25:09 -0600

    Inertial Sensor class Begun

--- 2131YPushBack/include/Chassis/Sensors/InertialSensor.hpp
+++ 2131YPushBack/include/Chassis/Sensors/InertialSensor.hpp
@@ -12,5 +12,18 @@
 
 #pragma once
 
+#include "pros/imu.hpp"
 #include <ostream>
+
+#include "Util/Positioning/Angle.hpp"
+
+class InertialSensor : pros::Imu {
+    public:
+        /**
+         * @brief Return angle of inertial sensor wrapped to 0-360 (0-2PI)
+         * 
+         * @return Angle 
+         */
+        Angle angle(); 
+};
 
