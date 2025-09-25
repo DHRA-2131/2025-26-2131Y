e4430fa476854ad109e00975eec543dbbc68db86
27qbracken09 <96312942+27qbracken09@users.noreply.github.com>
2025-09-25 14:54:09 -0600

    Did I fix it?

--- 2131YPushBack/src/Chassis/Sensors/MotorEncoder.cpp
+++ 2131YPushBack/src/Chassis/Sensors/MotorEncoder.cpp
@@ -1,7 +1,11 @@
 #include "Chassis/Sensors/MotorEncoder.hpp"
 #include "pros/motors.hpp"
 
-MotorEncoder(pros::MotorGroup& motors, float wheelRadius, float wheelOffset, float wheelGearing)
-    : AbstractEncoder(wheelRadius, wheelOffset) m_encoder(motors), m_wheelGearing(wheelGearing)
+MotorEncoder::MotorEncoder(pros::MotorGroup& motors, float wheelRadius, float wheelOffset, float wheelGearing)
+    : AbstractEncoder(wheelRadius, wheelOffset), m_encoder(motors), m_wheelGearing(wheelGearing)
 {}
 
+void MotorEncoder::reset()
+{
+    m_encoder.tare_position();
+}
