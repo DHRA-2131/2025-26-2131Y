a001badbcb1e6a52a48051c9d4616f92403c3055
Ezra Jones <120685907+ezraJones@users.noreply.github.com>
2025-10-07 16:57:36 -0600

    push

--- 2131YPushBack/src/Chassis/Sensors/MotorEncoder.cpp
+++ 2131YPushBack/src/Chassis/Sensors/MotorEncoder.cpp
@@ -1,9 +1,15 @@
 #include "Chassis/Sensors/MotorEncoder.hpp"
 #include "pros/motors.hpp"
+#include "Util/MathFunctions.hpp"
 
 MotorEncoder::MotorEncoder(pros::MotorGroup& motors, float wheelRadius, float wheelOffset, float wheelGearing)
     : AbstractEncoder(wheelRadius, wheelOffset), m_encoder(motors), m_wheelGearing(wheelGearing)
 {}
+
+float MotorEncoder::getPosition() const
+{
+    return static_cast<float>(average<double>({m_encoder.get_position(0), m_encoder.get_position(1), m_encoder.get_position(2)}));
+}
 
 void MotorEncoder::reset()
 {
